f9c7c42e0a27d6cef0943a7ff7df7a335037aea9
mehedi hasan <mehedihasan.j135@gmail.com>
2026-05-03 18:08:21 +0600

    fix some error

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://book-swap-gilt-tau.vercel.app/",
 });
 export const { signIn, signUp, useSession } = createAuthClient();
